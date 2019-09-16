406c9351ccd66a7070361e052c443c80293160e6
MAK JUNIOR <makjunior92@gmail.com>
2019-09-16 20:03:08 +0600

    upto depth burial

---
+++ js/calculator.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+// $('#calculate').click(function(){
+
+
+  
+
+  
+// });
+
+
+
+
+
+
+
+
+// var forms = document.getElementsByClassName('needs-validation');
+// var claulate = document.getElementById('calculate')
+// // Loop over them and prevent submission
+// var validation = Array.prototype.filter.call(forms, function (form) {
+//     calculate.addEventListener('click', function (event) {
+//         if (form.checkValidity() === false) {
+//             event.preventDefault();
+//             event.stopPropagation();
+            
+
+        
+//         }else{
+
+//           // calculator logic 
+         
+
+//         }
+        
+//         form.classList.add('was-validated');
+        
+
+
+        
+
+//     }, false);
+// });
+
+
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+//   });
+
+
+
+
