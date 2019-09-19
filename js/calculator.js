dc392ad2503adeb1252e76e3923da7f9ea13bfc0
MAK JUNIOR <makjunior92@gmail.com>
2019-09-20 01:46:20 +0600

    added javascript

--- js/calculator.js
+++ js/calculator.js
@@ -51,5 +51,124 @@
 //   });
 
 
+// var deflectionController;
+
+
+
+// var UIController = (function() {
+
+   
+    
+//     return {
+//         getInput: function() {
+//             return {
+//                 time_lag_factor_option: $('input[name=time_lag_factor_option]:checked').val()
+//             };
+//         },
+
+//          setEvents : {
+
+        
+//             time_lag_factor : {
+    
+//                 displayPressurezedTimeframe : function(){
+                   
+//                     $('#garvity_sewer_wrapper').hide();
+//                     $('#pressurized_timeframe_wrapper').show();
+    
+//                 },
+    
+//                 displayGravitySewerPipeTable: function(){
+    
+//                     $('#pressurized_timeframe_wrapper').hide();
+//                     $('#garvity_sewer_wrapper').show();
+    
+    
+    
+//                 }
+//             },
+    
+    
+    
+    
+    
+    
+//         },
+//     };
+
+
+// })();   
+
+
+
+
+// var controller = (function(UICtrl){
+
+//     $('#calculate').on('click',function(){
+
+//         console.log(UICtrl.getInput());
+
+//        if( UICtrl.getInput().value == 'pressure_pipe'){
+
+//                  UICtrl.setEvents.time_lag_factor.displayPressurezedTimeframe();
+
+       
+//        } 
+//        else{
+//                  UICtrl.setEvents.time_lag_factor.displayGravitySewerPipeTable();
+
+//        }
+
+//     });
+
+// })(UIController);
+
+
+
+$('#pressure_pipe').on('click', function(){
+
+    $('#gravity_sewer_wrapper').hide();
+    $('#pressurized_timeframe_wrapper').show();
+
+});
+
+
+
+
+$('#gravity_sewer_pipe').on('click', function(){
+
+    $('#pressurized_timeframe_wrapper').hide();
+    $('#gravity_sewer_wrapper').show();
+
+});
+
+
+$('#modulus1').on('click', function(){
+
+    $('#psi_for_modulus1_wrapper').show();
+    $('#psi_for_modulus2_wrapper').hide();
+    $('#psi_for_custom_modulus_wrapper').hide();
+
+});
+
+
+$('#modulus2').on('click', function(){
+
+    $('#psi_for_modulus1_wrapper').hide();
+    $('#psi_for_modulus2_wrapper').show();
+    $('#psi_for_custom_modulus_wrapper').hide();
+
+});
+
+$('#custom_modulus').on('click', function(){
+
+    $('#psi_for_modulus1_wrapper').hide();
+    $('#psi_for_modulus2_wrapper').hide();
+    $('#psi_for_custom_modulus_wrapper').show();
+
+});
+
+
+
 
 
