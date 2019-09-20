1cb9302ce29d962c4a906dc0d15b1fda6118b582
MAK JUNIOR <makjunior92@gmail.com>
2019-09-20 14:38:25 +0600

    added section visibility

--- js/calculator.js
+++ js/calculator.js
@@ -51,124 +51,256 @@
 //   });
 
 
-// var deflectionController;
 
 
 
-// var UIController = (function() {
 
-   
-    
-//     return {
-//         getInput: function() {
-//             return {
-//                 time_lag_factor_option: $('input[name=time_lag_factor_option]:checked').val()
-//             };
-//         },
 
-//          setEvents : {
 
+
+
+
+
+
+var calculateDeflection;
+
+
+
+var UIController = (function () {
+
+
+    return {
+
+            // Start:: section visibility
         
-//             time_lag_factor : {
-    
-//                 displayPressurezedTimeframe : function(){
-                   
-//                     $('#garvity_sewer_wrapper').hide();
-//                     $('#pressurized_timeframe_wrapper').show();
-    
-//                 },
-    
-//                 displayGravitySewerPipeTable: function(){
-    
-//                     $('#pressurized_timeframe_wrapper').hide();
-//                     $('#garvity_sewer_wrapper').show();
-    
-    
-    
-//                 }
-//             },
-    
-    
-    
-    
-    
-    
-//         },
-//     };
-
-
-// })();   
+     setSectionVisibility: {
 
 
 
+            time_lag_factor: {
 
-// var controller = (function(UICtrl){
+                display_pressurezed_timeframe: $('#pressure_pipe').on('click', function () {
 
-//     $('#calculate').on('click',function(){
+                    $('#gravity_sewer_wrapper').hide();
+                    $('#pressurized_timeframe_wrapper').show();
 
-//         console.log(UICtrl.getInput());
+                }),
 
-//        if( UICtrl.getInput().value == 'pressure_pipe'){
+                display_gravity_sewer_pipe_table: $('#gravity_sewer_pipe').on('click', function () {
 
-//                  UICtrl.setEvents.time_lag_factor.displayPressurezedTimeframe();
+                    $('#pressurized_timeframe_wrapper').hide();
+                    $('#gravity_sewer_wrapper').show();
 
-       
-//        } 
-//        else{
-//                  UICtrl.setEvents.time_lag_factor.displayGravitySewerPipeTable();
+                }),
+            },
 
-//        }
 
+            pipe_stiffness: {
+
+
+                display_psi_for_modulus1: $('#modulus1').on('click', function () {
+
+                    $('#psi_for_modulus1_wrapper').show();
+                    $('#psi_for_modulus2_wrapper').hide();
+                    $('#psi_for_custom_modulus_wrapper').hide();
+
+                }),
+
+                display_psi_for_modulus2: $('#modulus2').on('click', function () {
+
+                    $('#psi_for_modulus1_wrapper').hide();
+                    $('#psi_for_modulus2_wrapper').show();
+                    $('#psi_for_custom_modulus_wrapper').hide();
+
+                }),
+
+
+                display_psi_for_custom_modulus: $('#custom_modulus').on('click', function () {
+
+                    $('#psi_for_modulus1_wrapper').hide();
+                    $('#psi_for_modulus2_wrapper').hide();
+                    $('#psi_for_custom_modulus_wrapper').show();
+
+                })
+
+
+
+            },
+
+
+            liveLoad: {
+
+                hide_max_wheel_and_pipe_diameter: $('#no_live_load, #highway, #railway, #hs20_loading, #hs25_loading, #coopers_e80, #airport').on('click', function () {
+
+                    $('#max_wheel_and_pipe_diameter_wrapper').hide();
+
+                }),
+
+                display_max_wheel_and_pipe_diameter: $('#single_wheel_load, #two_passing_trucks').on('click', function () {
+
+                    $('#max_wheel_and_pipe_diameter_wrapper').show();
+
+                })
+
+            },
+
+
+
+            natural_or_in_place_soil_modulus: {
+
+                display_ucs_table: $('#unconfined_compressive_strength').on('click', function () {
+
+                    $('#ucs_table_wrapper').show();
+                    $('#spt_table_wrapper').hide();
+
+
+                }),
+
+                display_spt_table: $('#spt').on('click', function () {
+
+                    $('#ucs_table_wrapper').hide();
+                    $('#spt_table_wrapper').show();
+
+
+                })
+
+            }
+
+
+
+        },
+
+
+        // End:: section visibility
+
+
+        getInput: function () {
+           
+            return {
+                time_lag_factor_option: $('input[name=time_lag_factor_option]:checked').val()
+            };
+        },
+
+
+
+
+
+
+
+
+    };
+
+
+})();
+
+
+
+
+var controller = (function (UICtrl) {
+
+    $('#calculate').on('click', function () {
+
+        console.log(UICtrl.getInput());
+
+
+    });
+
+})(UIController);
+
+
+
+
+
+
+// (function setEvents(){
+
+
+//     // showing and hiding divs
+
+//     $('#pressure_pipe').on('click', function(){
+
+//         $('#gravity_sewer_wrapper').hide();
+//         $('#pressurized_timeframe_wrapper').show();
+    
 //     });
 
-// })(UIController);
 
 
 
-$('#pressure_pipe').on('click', function(){
+//     $('#gravity_sewer_pipe').on('click', function(){
 
-    $('#gravity_sewer_wrapper').hide();
-    $('#pressurized_timeframe_wrapper').show();
+//         $('#pressurized_timeframe_wrapper').hide();
+//         $('#gravity_sewer_wrapper').show();
+    
+//     });
+    
+    
+//     $('#modulus1').on('click', function(){
+    
+//         $('#psi_for_modulus1_wrapper').show();
+//         $('#psi_for_modulus2_wrapper').hide();
+//         $('#psi_for_custom_modulus_wrapper').hide();
+    
+//     });
+    
+    
+//     $('#modulus2').on('click', function(){
+    
+//         $('#psi_for_modulus1_wrapper').hide();
+//         $('#psi_for_modulus2_wrapper').show();
+//         $('#psi_for_custom_modulus_wrapper').hide();
+    
+//     });
+    
+//     $('#custom_modulus').on('click', function(){
+    
+//         $('#psi_for_modulus1_wrapper').hide();
+//         $('#psi_for_modulus2_wrapper').hide();
+//         $('#psi_for_custom_modulus_wrapper').show();
+    
+//     });
 
-});
+    
+//     $('#no_live_load, #highway, #railway, #hs20_loading, #hs25_loading, #coopers_e80, #airport').on('click', function(){
+    
+//         $('#max_wheel_and_pipe_diameter_wrapper').hide();
+       
+//     });
+
+//     $('#single_wheel_load, #two_passing_trucks').on('click', function(){
+    
+//         $('#max_wheel_and_pipe_diameter_wrapper').show();
+       
+//     });
+
+
+//     $('#unconfined_compressive_strength').on('click', function(){
+    
+//         $('#ucs_table_wrapper').show();
+//         $('#spt_table_wrapper').hide();
+
+       
+//     });
+
+
+//     $('#spt').on('click', function(){
+    
+//         $('#ucs_table_wrapper').hide();
+//         $('#spt_table_wrapper').show();
+
+       
+//     });
 
 
 
 
-$('#gravity_sewer_pipe').on('click', function(){
 
-    $('#pressurized_timeframe_wrapper').hide();
-    $('#gravity_sewer_wrapper').show();
-
-});
-
-
-$('#modulus1').on('click', function(){
-
-    $('#psi_for_modulus1_wrapper').show();
-    $('#psi_for_modulus2_wrapper').hide();
-    $('#psi_for_custom_modulus_wrapper').hide();
-
-});
-
-
-$('#modulus2').on('click', function(){
-
-    $('#psi_for_modulus1_wrapper').hide();
-    $('#psi_for_modulus2_wrapper').show();
-    $('#psi_for_custom_modulus_wrapper').hide();
-
-});
-
-$('#custom_modulus').on('click', function(){
-
-    $('#psi_for_modulus1_wrapper').hide();
-    $('#psi_for_modulus2_wrapper').hide();
-    $('#psi_for_custom_modulus_wrapper').show();
-
-});
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+// })();
 
